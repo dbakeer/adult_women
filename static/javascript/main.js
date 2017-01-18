7d29b1b7c7abcd5e2a834e0165fedfa7efa08e36
Dinah Bakeer <dinah.bakeer@gmail.com>
2017-01-18 15:37:07 -0500

    lots of alterations

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,30 +1,36 @@
-// var themeArray = [
-//   'nil',
-//   'transparent',
-//   'light',
-//   'dark',
-//   'dark',
-//   'light',
-//   'dark'
-// ];
+$('html').hide();
+
+$(window).load(function() {
+  $('html').fadeIn(3000);
+});
 
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-      anchors:[ 'adult-women', 'about', 'stories', 'photos', 'merchandise', 'contact'],
-      menu: '#menu',
-      continuousHorizontal: 'true',
-      responsiveWidth: 660,
-      scrollOverflow: true,
-      allowPageScroll: true,
-      paddingTop: 24,
-      fixedElements: '#menu',
-      slideSelector: '.horizontal-scrolling'
-      // onLeave: function(index, nextIndex, direction) {
-      //   var theme = themeArray[nextIndex];
-      //   $header.attr('data-theme', theme);
-      //   $header.addClass('active');
-      //   $('.event-date').addClass('active');
-      //   $('#fp-nav').addClass('mobile-active');
-      // }
-    });
+  $('#fullpage').fullpage({
+    anchors:[
+      'adult-women',
+      'about',
+      'stories',
+      'photos',
+      'merchandise',
+      'contact'
+    ],
+    menu: '#menu',
+    continuousHorizontal: 'true',
+    responsiveWidth: 660,
+    scrollOverflow: true,
+    allowPageScroll: true,
+    paddingTop: 24,
+    fixedElements: '#menu',
+    slideSelector: '.horizontal-scrolling',
+    afterRender: function() {
+      $('#menu').hide();
+    },
+    afterLoad: function(anchor, index){
+      if(index == 1) {
+        $('#menu').fadeOut();
+      } else {
+        $('#menu').fadeIn();
+      }
+    }
   });
+});
